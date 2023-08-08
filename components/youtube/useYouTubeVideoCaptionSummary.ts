30f555616c9edd48fe7f02e4c00f237093216770
Oscar <tangqh@sea.com>
2023-08-08 13:57:19 +0800

    fix: improve summary prompts

--- components/youtube/useYouTubeVideoCaptionSummary.ts
+++ components/youtube/useYouTubeVideoCaptionSummary.ts
@@ -2,34 +2,25 @@ import { GetPrompt, useSummary } from '@components/commons/useSummary';
 
 const getPrompt: GetPrompt = (title: string, caption: string, language: 'en' | 'zh-CN') => {
   return {
-    en: `I will provide you the title and caption of a video.
-Your task is to summarize the caption into key points that are most relevant to the title, and reply only the key points to me in English.
-You should reply in bullet point format. Your reply should be concise, easy to understand, and without any redundant content.
-Don't wrap your reply in XML tag.
+    en: `## Video Title ##
+${title}
 
-This is the video title, in <title></title> XML tag:
-<title>${title}</title>
-
-This is the video caption, in <caption></caption> XML tag:
-<caption>
+## Video Caption ##
 ${caption}
-</caption>
 
-Please reply in English.`,
+## Task ##
+Summarize the caption into a short list of key points (no more than 10) that are most relevant to the title, and reply in English.
+Your reply should be concise, easy to understand. You should reply without any redundant content, title, or description.`,
 
-    'zh-CN': `我会给你提供一个视频的标题和字幕内容。你的任务是从字幕中总结出与标题相关的主要关键点，并以中文回复。
-你的回复应当简洁、易懂、没有多余的内容。你的回复只需要包括主要关键点，无需包括视频标题、也无需额外说明。
-切勿将回复内容包含在XML标签里。
+    'zh-CN': `## 视频标题 ##
+${title}
 
-以下是视频标题，在 <title></title> XML 标签中:
-<title>${title}</title>
-
-以下是视频字幕，在 <transcription></transcription> XML 标签中:
-<transcription>
+## 视频字幕 ##
 ${caption}
-</transcription>
 
-请务必以中文回复！`,
+## 任务 ##
+将字幕总结成跟标题最为相关的最主要的关键点（不多于10个），并以中文回复。
+你的总结应简洁、易懂、没有多余的内容、不用带标题或描述。`,
   }[language]!;
 };
 
--- components/youtube/useYouTubeVideoCommentsSummary.ts
+++ components/youtube/useYouTubeVideoCommentsSummary.ts
@@ -4,35 +4,25 @@ import { GetPrompt, useSummary } from '@components/commons/useSummary';
 
 const getPrompt: GetPrompt = (title: string, comments: string, language: 'en' | 'zh-CN') => {
   return {
-    en: `I will provide you the title and viewers' comments of a video.
-Your task is to summarize the comments into bullet points representing major opinions of the viewers.
-You should include at most the top 5 major opinions in the summary.
-You should reply in bullet point format. Your reply should be concise, easy to understand, and without any redundant content.
-Don't wrap your reply in XML tag. You should never simply translate each comment!
+    en: `## Video Title ##
+${title}
 
-This is the video title, in <title></title> XML tag:
-<title>${title}</title>
-
-These are viewers' comments, in <comments></comments> XML tag:
-<comments>
+## Viewers' Comments ##
 ${comments}
-</comments>
 
-Please reply in English.`,
+## Task ##
+Summarize the comments into a short list of most relevant key points, and reply in English.
+Your reply should be concise, easy to understand. You should reply without any redundant content, title, or description.`,
 
-    'zh-CN': `我会给你提供一个视频的标题和用户评论。你的任务是从评论中总结出用户的主要观点，并以中文回复。
-你在回复里只需要包括最多5个最主要观点即可。你的回复应当简洁、易懂、没有多余的内容。你的回复只需要包括主要观点，无需包括视频标题、也无需额外说明和描述。
-切勿将回复内容包含在XML标签里。
+    'zh-CN': `## 视频标题 ##
+${title}
 
-以下是视频标题，在 <title></title> XML 标签中:
-<title>${title}</title>
-
-以下是用户评论，在 <comments></comments> XML 标签中:
-<comments>
+## 用户评论 ##
 ${comments}
-</comments>
 
-请务必以中文回复！`,
+## 任务 ##
+将用户评论总结成最主要的用户观点，并以中文回复。
+你的总结应简洁、易懂、没有多余的内容、不用带标题或描述。`,
   }[language];
 };
 
