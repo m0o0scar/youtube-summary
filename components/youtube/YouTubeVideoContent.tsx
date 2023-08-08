92c3db7c05fcd0a1abdca00d8c8b6cdd3ae92f2e
Oscar <tangqh@sea.com>
2023-08-08 14:24:43 +0800

    feat(ui): collapse caption & comments by default

--- components/youtube/YouTubeVideoContent.tsx
+++ components/youtube/YouTubeVideoContent.tsx
@@ -19,28 +19,31 @@ export const YouTubeVideoContent: FC<YouTubeVideoContentProps> = ({
   emptyPlaceholder = null,
 }) => {
   return (
-    <div className="flex flex-col gap-2">
-      {/* title & subtitle */}
-      <div className="flex gap-2 items-center">
-        <h3>{title}</h3>
-        {subtitle && <span className="text-xs text-slate-400">/ {subtitle}</span>}
-      </div>
-
-      {/* content */}
-      {status === 'pending' && <span className="loading loading-spinner" />}
-      {status !== 'pending' && (
-        <div className="p-4 shadow-inner rounded-xl text-sm sm:text-base text-slate-600 bg-slate-100 dark:text-slate-300 dark:bg-slate-700 overflow-auto max-h-64">
-          {status === 'empty' && emptyPlaceholder}
-          {status === 'loaded' && typeof content === 'string' && content}
-          {status === 'loaded' && isArray(content) && (
-            <ol className="m-0">
-              {content.map((item, i) => (
-                <li key={i}>{item}</li>
-              ))}
-            </ol>
-          )}
+    <div className="collapse bg-base-200 dark:bg-slate-700">
+      <input type="checkbox" />
+      <div className="collapse-title">
+        <div className="flex gap-2 items-center">
+          <h3>{title}</h3>
+          {subtitle && <span className="text-xs text-slate-400">/ {subtitle}</span>}
         </div>
-      )}
+      </div>
+      <div className="collapse-content">
+        {/* content */}
+        {status === 'pending' && <span className="loading loading-spinner" />}
+        {status !== 'pending' && (
+          <div className="text-sm sm:text-base text-slate-600 dark:text-slate-300 overflow-auto max-h-64">
+            {status === 'empty' && emptyPlaceholder}
+            {status === 'loaded' && typeof content === 'string' && content}
+            {status === 'loaded' && isArray(content) && (
+              <ol className="m-0 pl-8">
+                {content.map((item, i) => (
+                  <li key={i}>{item}</li>
+                ))}
+              </ol>
+            )}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
