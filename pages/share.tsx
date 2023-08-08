c537150997ac43472421d24b2d93e7ba73fe5231
Oscar <tangqh@sea.com>
2023-08-08 17:27:11 +0800

    fix: render SEO headers on server side

--- pages/share.tsx
+++ pages/share.tsx
@@ -1,16 +1,32 @@
-import { useSearchParams } from 'next/navigation';
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 
 import { Header } from '@components/commons/Header';
 import { YouTubeVideoShareCard } from '@components/youtube/YouTubeVideoShareCard';
 
-export default function Page() {
-  const searchParams = useSearchParams();
+interface SharePageProps {
+  url?: string;
+  thumbnail?: string;
+  title?: string;
+  content?: string;
+  duration?: string;
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const host = context.req.headers.host;
+  const path = context.req.url;
+  const { searchParams } = new URL(`${host}${path}`);
+
   const url = searchParams.get('u') as string;
   const thumbnail = searchParams.get('p') as string;
   const title = searchParams.get('t') as string;
   const content = searchParams.get('c') as string;
   const duration = searchParams.get('d') as string;
+  const props: SharePageProps = { url, thumbnail, title, content, duration };
 
+  return { props };
+};
+
+export default function Page({ url, thumbnail, title, content, duration }: SharePageProps) {
   return (
     <>
       <Header
