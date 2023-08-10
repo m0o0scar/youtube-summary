0e633838b7c9b867f3a51318eab72df752a5766f
Oscar <tangqh@sea.com>
2023-08-10 18:05:17 +0800

    refactor: standardize prompt format

--- components/youtube/useYouTubeVideoCaptionSummary.ts
+++ components/youtube/useYouTubeVideoCaptionSummary.ts
@@ -2,24 +2,26 @@ import { GetPrompt, useSummary } from '@components/commons/useSummary';
 
 const getPrompt: GetPrompt = (title: string, caption: string, language: 'en' | 'zh-CN') => {
   return {
-    en: `## Video Title ##
-${title}
+    en: `This is the title of a video, in <title></title> XML tag:
+<title>${title}</title>
 
-## Video Caption ##
+This is the caption of the video, in <caption></caption> XML tag:
+<caption>
 ${caption}
+</caption>
 
-## Task ##
-Summarize the caption into a short paragraph (within 10 sentences), and reply in English.
+Your task is to summarize the caption into a short paragraph (within 10 sentences), and reply in English.
 Your reply should be concise, easy to understand. You should reply without any redundant content like title or description.`,
 
-    'zh-CN': `## 视频标题 ##
-${title}
+    'zh-CN': `这是一个视频的标题，在<title></title> XML标签里：
+<title>${title}</title>
 
-## 视频字幕 ##
+这是该视频的字幕，在<caption></caption> XML标签里：
+<caption>
 ${caption}
+</caption>
 
-## 任务 ##
-将字幕总结成一段概括字幕主要内容、事实、和观点的短文（不超过十句话），并以中文回复。你的总结应简洁和易懂，不用带标题或描述等多余内容。
+你的任务是将字幕总结成一段概括字幕主要内容、事实、和观点的短文（不超过十句话），并以中文回复。你的总结应简洁和易懂，不用带标题或描述等多余内容。
 重要的事情说三遍：请以中文回复，请以中文回复，请以中文回复！`,
   }[language]!;
 };
--- components/youtube/useYouTubeVideoCommentsSummary.ts
+++ components/youtube/useYouTubeVideoCommentsSummary.ts
@@ -4,32 +4,34 @@ import { GetPrompt, useSummary } from '@components/commons/useSummary';
 
 const getPrompt: GetPrompt = (title: string, comments: string, language: 'en' | 'zh-CN') => {
   return {
-    en: `## Video Title ##
-${title}
+    en: `This is the title of a video, in <title></title> XML tag:
+<title>${title}</title>
 
-## Viewers' Comments ##
+These are viewers' comments to the video, in <comments></comments> XML tag:
+<comments>
 ${comments}
+</comments>
 
-## Task ##
-Summarize the comments into a short list of most relevant key points, and reply in English.
+Your task is to summarize the comments into a short list of most relevant key points (no more than 5), and reply in English.
 Your reply should be concise, easy to understand. You should reply without any redundant content, title, or description.
 
-## Example Reply ##
+Example Reply:
 - Most viewers think that ...
 - Some viewers believe ...
 - Overall, ...`,
 
-    'zh-CN': `## 视频标题 ##
-${title}
+    'zh-CN': `这是一个视频的标题，在<title></title> XML标签里：
+<title>${title}</title>
 
-## 用户评论 ##
+这是用户对该视频的评论，在<comments></comments> XML标签里：
+<comments>
 ${comments}
+</comments>
 
-## 任务 ##
-将用户评论总结成最主要的用户观点，并以中文回复。
+你的任务是将用户评论总结成最主要的用户观点（不超过5个），并以中文回复。
 你的总结应简洁、易懂、没有多余的内容，不用带标题或描述。
 
-## 示例回复 ##
+示例回复：
 - 大部分人认为...
 - 有些人认为...
 - 总体来说...`,
