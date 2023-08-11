7dbb01e9a16aae4008e6bdf3235edd88a4ff7bdd
Oscar <tangqh@sea.com>
2023-08-11 14:10:53 +0800

    fix: wrong number format calculation

--- components/utils/number.ts
+++ components/utils/number.ts
@@ -1,5 +1,5 @@
 export function formatNumberShort(n: number, base = 1000) {
-  if (n > base * base) return `${((n / base) * base).toFixed(1)}M`;
+  if (n > base * base) return `${(n / base / base).toFixed(1)}M`;
   if (n > base) return `${(n / base).toFixed(1)}K`;
   return n.toString();
 }
