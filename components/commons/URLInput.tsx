ff5b3bfbc097ee3ca3d139359825d0e6adcb6f59
Oscar <tangqh@sea.com>
2023-08-16 11:15:38 +0800

    fix: hide url input to avoid double requests issue

--- components/commons/URLInput.tsx
+++ components/commons/URLInput.tsx
@@ -61,6 +61,8 @@ export const URLInput: FC<URLInputProps> = ({ language, disabled, onSupportedURL
     }
   }, [urlFromSearchParams]);
 
+  return null;
+
   return (
     <div className="flex gap-2 items-end">
       {/* URL input */}
