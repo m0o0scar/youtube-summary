e9a4d07c24b648ef228e966d3c6b452ac36813e2
Oscar <tangqh@sea.com>
2023-08-28 13:15:59 +0800

    feat: enable vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,20 @@ import '../styles/globals.css';
 
 import { SessionProvider } from 'next-auth/react';
 
+import { Analytics } from '@vercel/analytics/react';
+
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <div className="font-sans">
-        <Component {...pageProps} />
-      </div>
-    </SessionProvider>
+    <>
+      <SessionProvider session={session}>
+        <div className="font-sans">
+          <Component {...pageProps} />
+        </div>
+      </SessionProvider>
+      <Analytics />
+    </>
   );
 }
 
