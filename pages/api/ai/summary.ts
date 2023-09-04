6db3c40b7afbb0f9ddcb116506edd95812b8ee95
Oscar <tangqh@sea.com>
2023-09-04 11:50:46 +0800

    feat: support streaming on client

--- pages/api/ai/summary.ts
+++ pages/api/ai/summary.ts
@@ -1,4 +1,5 @@
 import { StreamingTextResponse } from 'ai';
+import { ChatAnthropic } from 'langchain/chat_models/anthropic';
 import { ChatOpenAI } from 'langchain/chat_models/openai';
 import { PromptTemplate } from 'langchain/prompts';
 import { BytesOutputParser } from 'langchain/schema/output_parser';
@@ -7,12 +8,16 @@ import { NextRequest } from 'next/server';
 export const runtime = 'edge';
 
 export default async function handler(req: NextRequest) {
-  const { prompt, temperature = 0.5, maxReplyTokens = 1024 } = await req.json();
+  const { messages } = await req.json();
+  const prompt = messages[0].content;
+  const temperature = 0.5;
+  const maxReplyTokens = 1024;
 
   const promptTemplate = PromptTemplate.fromTemplate('{prompt}');
 
   const options = {
     openAIApiKey: process.env.OPENAI_API_TOKEN,
+    anthropicApiKey: process.env.ANTHROPIC_API_TOKEN,
     temperature,
     maxTokens: maxReplyTokens,
   };
@@ -20,6 +25,7 @@ export default async function handler(req: NextRequest) {
   const models = [
     { maxTotalToken: 4, model: new ChatOpenAI({ modelName: 'gpt-3.5-turbo', ...options }) },
     { maxTotalToken: 16, model: new ChatOpenAI({ modelName: 'gpt-3.5-turbo-16k', ...options }) },
+    { maxTotalToken: 100, model: new ChatAnthropic({ modelName: 'claude-instant-1', ...options }) },
   ];
 
   /**
@@ -42,7 +48,12 @@ export default async function handler(req: NextRequest) {
     if (canHandlePromot) {
       const chain = promptTemplate.pipe(model).pipe(outputParser);
       const stream = await chain.stream({ prompt });
-      return new StreamingTextResponse(stream);
+      return new StreamingTextResponse(stream, {
+        headers: {
+          'llm-provider': model.modelName.startsWith('gpt-') ? 'openai' : 'anthropic',
+          'llm-model': model.modelName,
+        },
+      });
     }
   }
 
