e23b81c005d383e20e885bedb5a7e49d6567a68a
Oscar Tong <moscartong@gmail.com>
2023-09-08 22:20:08 +0800

    fix: keep only limited amount of history messages

--- components/commons/useChatContent.tsx
+++ components/commons/useChatContent.tsx
@@ -1,3 +1,4 @@
+import { Message } from 'ai';
 import { useChat } from 'ai/react';
 import { useEffect, useState } from 'react';
 
@@ -6,6 +7,7 @@ const getId = () => (++id).toString();
 
 export const useChatContent = (prompt: string, api = '/api/ai/chat') => {
   const [pending, setPending] = useState(false);
+
   const { messages, setMessages, reload } = useChat({
     api,
     onFinish: () => setPending(false),
@@ -13,7 +15,16 @@ export const useChatContent = (prompt: string, api = '/api/ai/chat') => {
 
   const sendMessage = (content: string) => {
     setPending(true);
-    setMessages([...messages, { id: getId(), role: 'user', content }]);
+
+    const newMessage: Message = { id: getId(), role: 'user', content };
+    const maxHistoryPair = 5;
+    if (messages.length <= maxHistoryPair * 2) {
+      setMessages([...messages, newMessage]);
+    } else {
+      const [systemMessage, ...history] = messages;
+      setMessages([systemMessage, ...history.slice(-maxHistoryPair * 2), newMessage]);
+    }
+
     reload();
   };
 
