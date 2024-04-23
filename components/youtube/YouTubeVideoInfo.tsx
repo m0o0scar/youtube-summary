79c36ca0d8c4c54ddb6aaba85fcaf664c5e033c8
Oscar <tangqh@sea.com>
2024-04-23 09:20:42 +0800

    feat: update page title with youtube video title

--- components/youtube/YouTubeVideoInfo.tsx
+++ components/youtube/YouTubeVideoInfo.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import { FC } from 'react';
 
+import { Header } from '@components/commons/Header';
 import { formatNumberShort, percentage } from '@components/utils/number';
 
 import { YouTubeChannel } from './type';
@@ -29,32 +30,35 @@ export const YouTubeVideoInfoCard: FC<YouTubeVideoInfoProps> = ({
   if (commentCount) subtitle.push(`💬 ${formatNumberShort(commentCount)}`);
 
   return (
-    <div className="flex flex-col sm:flex-row gap-4 items-start">
-      {/* thumbnail */}
-      {!thumbnail && <div className="w-full sm:w-60 rounded-lg aspect-video bg-slate-200" />}
-      {thumbnail && (
-        <img
-          src={thumbnail}
-          alt="thumbnail"
-          className="m-0 w-full sm:w-60 rounded-lg aspect-video object-cover"
-          referrerPolicy="no-referrer"
-        />
-      )}
-
-      {/* info */}
-      <div className="flex-1 flex flex-col gap-2 w-full">
-        {/* title */}
-        {!title && <span className="loading loading-spinner" />}
-        {title && (
-          <a className="no-underline line-clamp-2" href={url} target="_blank">
-            <div className="text-lg sm:text-xl">{title}</div>
-          </a>
+    <>
+      {title && <Header title={`Summary of "${title}"`} />}
+      <div className="flex flex-col sm:flex-row gap-4 items-start">
+        {/* thumbnail */}
+        {!thumbnail && <div className="w-full sm:w-60 rounded-lg aspect-video bg-slate-200" />}
+        {thumbnail && (
+          <img
+            src={thumbnail}
+            alt="thumbnail"
+            className="m-0 w-full sm:w-60 rounded-lg aspect-video object-cover"
+            referrerPolicy="no-referrer"
+          />
         )}
 
-        {subtitle.length > 0 && <div className="text-sm">{subtitle.join(', ')}</div>}
-        {subtitle.length > 0 && channel && <hr className="m-1" />}
-        {channel && <YouTubeChannelInfo channel={channel} />}
+        {/* info */}
+        <div className="flex-1 flex flex-col gap-2 w-full">
+          {/* title */}
+          {!title && <span className="loading loading-spinner" />}
+          {title && (
+            <a className="no-underline line-clamp-2" href={url} target="_blank">
+              <div className="text-lg sm:text-xl">{title}</div>
+            </a>
+          )}
+
+          {subtitle.length > 0 && <div className="text-sm">{subtitle.join(', ')}</div>}
+          {subtitle.length > 0 && channel && <hr className="m-1" />}
+          {channel && <YouTubeChannelInfo channel={channel} />}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
